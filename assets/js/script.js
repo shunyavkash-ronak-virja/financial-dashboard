ab25da430676e77016a9ad1f9a5991dc05489bef
Ronak Virja <ronak.virja@shunyavkash.com>
2026-09-17 17:54:34 +0530

    created chart section and fixing bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,64 @@ menuBtn.addEventListener("click", () => {
 asideCloseBtn.addEventListener("click", () => {
     aside.classList.remove("active");
 });
+
+
+// Investment Performance Chart -----
+const investmentChart = document.getElementById('investmentChart');
+const investmentChartInstance = new Chart(investmentChart, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jan', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets: [{
+            label: 'Current Value',
+            data: [2, 12, 9, 16, 8, 10, 12, 16, 12, 20, 10, 12],
+            borderWidth: 1,
+            backgroundColor: ["#0a2647"],
+        },
+        {
+            label: 'Initial Investment',
+            data: [2, 12, 9, 16, 10, 6, 4, 20, 18, 20, 22, 16],
+            borderWidth: 1,
+            backgroundColor: ["#5f7184"],
+        }]
+    },
+    options: {
+        responsive: true,
+        aspectRatio: 1.4,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+    }
+});
+
+const assetAllocationChart = document.getElementById('assetAllocationChart');
+const assetAllocationChartInstance = new Chart(assetAllocationChart, {
+    type: 'pie',
+    data: {
+        labels: ['Stocks', 'Gold', 'Cash'],
+        datasets: [{
+            label: 'Current Value',
+            data: [40, 40, 20],
+            borderWidth: 1,
+            backgroundColor: ["#163b63", "#415a75", "#243b53"],
+        },
+        ]
+    },
+    options: {
+        responsive: true,
+        aspectRatio: 1.4,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+window.addEventListener('resize', () => {
+    investmentChartInstance.resize();
+    assetAllocationChartInstance.resize();
+});
